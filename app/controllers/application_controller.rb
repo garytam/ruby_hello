e99a1ea70f84006792611c8b72fff1d5142de1f8
Gary Tam <gary.c.tam@gmail.com>
2019-04-13 09:03:36 -0400

    new text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render html: "Hey you"
+  	render html: "Hey you dodo bird"
   end
 
 end
